1c8f4c8181333eb8f45797e0ebf33f6fd32411ab
akaysh <ksh.shrm1@gmail.com>
2021-09-20 03:26:11 -0400

    Data Preprocessing and Training params

--- scripts/train.sh
+++ scripts/train.sh
@@ -8,25 +8,27 @@ export NUMPY_SEED=`expr $PYTORCH_SEED / 10`
 export BERT_VOCAB=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scivocab_uncased.vocab
 export BERT_WEIGHTS=https://ai2-s2-research.s3-us-west-2.amazonaws.com/scibert/allennlp_files/scibert_scivocab_uncased.tar.gz
 
+LABEL_KEY=coarse
+
 # path to dataset files
-export TRAIN_PATH=data/CSAbstruct/train.jsonl
-export DEV_PATH=data/CSAbstruct/dev.jsonl
-export TEST_PATH=data/CSAbstruct/test.jsonl
+export TRAIN_PATH=data/Discourse/train_$LABEL_KEY.jsonl
+export DEV_PATH=data/Discourse/dev$LABEL_KEY.jsonl
+export TEST_PATH=data/Discourse/test$LABEL_KEY.jsonl
 
 # model
-export USE_SEP=true  # true for our model. false for baseline
-export WITH_CRF=false  # CRF only works for the baseline
+export USE_SEP=false  # true for our model. false for baseline
+export WITH_CRF=true  # CRF only works for the baseline
 
 # training params
 export cuda_device=0
 export BATCH_SIZE=4
 export LR=5e-5
-export TRAINING_DATA_INSTANCES=1668
-export NUM_EPOCHS=2
+export TRAINING_DATA_INSTANCES=209
+export NUM_EPOCHS=20
 
 # limit number of sentneces per examples, and number of words per sentence. This is dataset dependant
-export MAX_SENT_PER_EXAMPLE=10
-export SENT_MAX_LEN=80
+export MAX_SENT_PER_EXAMPLE=20
+export SENT_MAX_LEN=50
 
 # this is for the evaluation of the summarization dataset
 export SCI_SUM=false
