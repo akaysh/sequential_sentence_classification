dc16da22f78ff3291c0d27c72efb888ef4c3e01f
akaysh <ksh.shrm1@gmail.com>
2021-09-26 00:31:45 -0400

    Updated the preprocessing script for new data

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
-# In[40]:
-
+# In[5]:
 
 import pandas as pd
 import glob
@@ -13,15 +12,14 @@ import os
 from pathlib import Path
 
 
-path_iclr_train = r'../0517_split_2/train/' # use your path
-path_iclr_test = r'../0517_split_2/test/' # use your path
-path_iclr_dev = r'../0517_split_2/dev/' # use your path
+path_iclr_train = r'../../Discourse_Final_Dataset/train/' # use your path
+path_iclr_test = r'../../Discourse_Final_Dataset/test/' # use your path
+path_iclr_dev = r'../../Discourse_Final_Dataset/dev/' # use your path
 
 label_name_classification = "coarse"
 
 
-# In[39]:
-
+# In[6]:
 
 # keys are label types here
 # label_keys = ["coarse","fine","asp","pol"]
@@ -30,8 +28,7 @@ label_keys = ["coarse"]
 
 # ## Dev Data
 
-# In[44]:
-
+# In[8]:
 
 all_review_files_dev = glob.glob(path_iclr_dev + "*")
 
@@ -41,22 +38,18 @@ for filename in all_review_files_dev:
     
   with open(filename, 'r') as f:
     data_json =  json.loads(f.read())
-  rev_df = pd.DataFrame(data_json["review"])
-  rev_label_df = pd.DataFrame(data_json["reviewlabels"])
-  df = pd.concat([rev_df,rev_label_df],axis=1)
-  df = df.loc[:,~df.columns.duplicated()]
-  data_li.append(df)
+  rev_df = pd.DataFrame(data_json["review_sentences"])
+  data_li.append(rev_df)
 
 
-# In[45]:
-
+# In[10]:
 
 for label_key in label_keys:
     json_li = []
     for data in data_li:
-        json_li.append({"review_id":data["text_id"][0],"sentences":[sentence for sentence in data["sentence"]],"labels":[req_label for req_label in [label[label_key] for label in data["labels"]]]})
-    Path("./data/Discourse").mkdir(parents=True, exist_ok=True)
-    with open(f'./data/Discourse/dev_{label_key}.jsonl', 'w') as outfile:
+        json_li.append({"review_id":data["review_id"][0],"sentences":[sentence for sentence in data["text"]],"labels":[req_label for req_label in data[label_key]]})
+    Path("../data/Discourse").mkdir(parents=True, exist_ok=True)
+    with open(f'../data/Discourse/dev_{label_key}.jsonl', 'w') as outfile:
         for entry in json_li:
             json.dump(entry, outfile)
             outfile.write('\n')
@@ -64,8 +57,7 @@ for label_key in label_keys:
 
 # ## Training Data
 
-# In[34]:
-
+# In[11]:
 
 all_review_files_train = glob.glob(path_iclr_train + "*")
 
@@ -75,23 +67,19 @@ for filename in all_review_files_train:
     
   with open(filename, 'r') as f:
     data_json =  json.loads(f.read())
-  rev_df = pd.DataFrame(data_json["review"])
-  rev_label_df = pd.DataFrame(data_json["reviewlabels"])
-  df = pd.concat([rev_df,rev_label_df],axis=1)
-  df = df.loc[:,~df.columns.duplicated()]
-  data_li.append(df)
+  rev_df = pd.DataFrame(data_json["review_sentences"])
+  data_li.append(rev_df)
 
 
-# In[43]:
-
+# In[12]:
 
 
 for label_key in label_keys:
     json_li = []
     for data in data_li:
-        json_li.append({"review_id":data["text_id"][0],"sentences":[sentence for sentence in data["sentence"]],"labels":[req_label for req_label in [label[label_key] for label in data["labels"]]]})
-    Path("./data/Discourse").mkdir(parents=True, exist_ok=True)
-    with open(f'./data/Discourse/train_{label_key}.jsonl', 'w') as outfile:
+        json_li.append({"review_id":data["review_id"][0],"sentences":[sentence for sentence in data["text"]],"labels":[req_label for req_label in data[label_key]]})
+    Path("../data/Discourse").mkdir(parents=True, exist_ok=True)
+    with open(f'../data/Discourse/train_{label_key}.jsonl', 'w') as outfile:
         for entry in json_li:
             json.dump(entry, outfile)
             outfile.write('\n')
@@ -99,40 +87,28 @@ for label_key in label_keys:
 
 # ## Testing Data
 
-# In[46]:
-
+# In[14]:
 
 all_review_files_test = glob.glob(path_iclr_test + "*")
 
 data_li = []
 
 for filename in all_review_files_test:
-    
-  with open(filename, 'r') as f:
-    data_json =  json.loads(f.read())
-  rev_df = pd.DataFrame(data_json["review"])
-  rev_label_df = pd.DataFrame(data_json["reviewlabels"])
-  df = pd.concat([rev_df,rev_label_df],axis=1)
-  df = df.loc[:,~df.columns.duplicated()]
-  data_li.append(df)
+    with open(filename, 'r') as f:
+        data_json =  json.loads(f.read())
+    rev_df = pd.DataFrame(data_json["review_sentences"])
+    data_li.append(rev_df)
 
 
-# In[47]:
-
+# In[15]:
 
 for label_key in label_keys:
     json_li = []
     for data in data_li:
-        json_li.append({"review_id":data["text_id"][0],"sentences":[sentence for sentence in data["sentence"]],"labels":[req_label for req_label in [label[label_key] for label in data["labels"]]]})
-    Path("./data/Discourse").mkdir(parents=True, exist_ok=True)
-    with open(f'./data/Discourse/test_{label_key}.jsonl', 'w') as outfile:
+        json_li.append({"review_id":data["review_id"][0],"sentences":[sentence for sentence in data["text"]],"labels":[req_label for req_label in data[label_key]]})
+    Path("../data/Discourse").mkdir(parents=True, exist_ok=True)
+    with open(f'../data/Discourse/test_{label_key}.jsonl', 'w') as outfile:
         for entry in json_li:
             json.dump(entry, outfile)
             outfile.write('\n')
-
-
-# In[ ]:
-
-
-
 
